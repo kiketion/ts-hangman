fb065817a6a32d0982d3a4b0202d6297b265e065
kiketion <kiketionje@gmail.com>
2022-11-04 16:19:10 -0600

    doll

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,34 @@
-function App() {
+import { useState } from 'react';
+import { Drawing } from './components/Drawing';
+import { Word } from './components/Word';
+import { Keyboard } from './components/Keyboard';
+import words from './wordList.json';
 
+function App() {
+  const [wordGuessed, setWordGessed] = useState(() => {
+    return words[Math.floor(Math.random() * words.length)];
+  });
+  const [gessedLetters, setGuessedLetters] = useState<string[]>([]);
+
+  console.log(wordGuessed);
 
   return (
-    <div>
-      <h1>
-        Hello TS
-      </h1>
+    <div
+      style={{
+        maxWidth: '800px',
+        display: 'flex',
+        flexDirection: 'column',
+        gap: '2rem',
+        margin: '0 auto',
+        alignItems: 'center',
+      }}
+    >
+      <div style={{ fontSize: '2rem', textAlign: 'center' }}>Lose Win</div>
+      <Drawing />
+      <Word />
+      <Keyboard />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
